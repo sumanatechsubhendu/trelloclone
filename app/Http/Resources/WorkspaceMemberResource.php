1528922c511a5fab83fb6cbe2cb7a70e10676941
manikandaprabu11602 <manikandaprabu.v@arkinfotec.com>
2024-05-22 11:17:04 +0530

    Workspace members API bug fix -158

--- app/Http/Resources/WorkspaceMemberResource.php
+++ app/Http/Resources/WorkspaceMemberResource.php
@@ -19,6 +19,7 @@ class WorkspaceMemberResource extends JsonResource
             'workspace_id' => $this->workspace_id,
             'user_id' => $this->user_id,
             'created_at' => $this->created_at,
+            'user' => new UserResource($this->user), // Assuming UserResource is another resource class
             'workspace' => new WorkspaceResource($this->workspace), // Assuming WorkspaceResource is another resource class
         ];
     }
