f1b97d0f843a78bad749e0c3e5a3718115791ed4
manikandaprabu11602 <manikandaprabu.v@arkinfotec.com>
2024-05-28 12:57:36 +0530

    Conflict issue fix

--- routes/api.php
+++ routes/api.php
@@ -29,6 +29,7 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
 Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::post('forget-password', [AuthController::class, 'forgetPassword']);
